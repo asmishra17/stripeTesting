f4e549b99ce38df18949b7062d80af54ae2648b0
aditi.mishra <asmishra93@gmail.com>
2021-03-11 11:11:10 -0800

    testing

--- test.js
+++ test.js
@@ -1,10 +1,13 @@
-// Set your secret key. Remember to switch to your live secret key in production.
-// See your keys here: https://dashboard.stripe.com/account/apikeys
-const stripe = require('stripe')('sk_test_51IAqJEGU6PkjlLbpOgamhO9BQ6hTYiIFWJimkC9KohmwBJeVsKnmBlZQGFNiBNX9PMYJP9Ktjjwr5NTGZyXjdFBp00Qym2DyJk');
+const stripe = require('stripe')('sk_test_BQokikJOvBiI2HlWgH4olfQ2');
 
-const paymentIntent = await stripe.paymentIntents.create({
-  amount: 1000,
+// Create a payment intent to start a purchase flow.
+let paymentIntent = await stripe.paymentIntents.create({
+  amount: 2000,
   currency: 'usd',
-  payment_method_types: ['card'],
-  receipt_email: 'jenny.rosen@example.com',
+  description: 'My first payment',
+});
+
+// Complete the payment using a test card.
+await stripe.paymentIntents.confirm(paymentIntent.id, {
+  payment_method: 'pm_card_mastercard',
 });
